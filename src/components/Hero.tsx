462785a65f2d20ebb19397b8e6d47c1a150867dc
Saputra <saputra.uta50@gmail.com>
2024-01-14 21:57:50 +0800

    Fixing navbar and hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import { TypeAnimation } from "react-type-animation";
 
 export default function Hero() {
   return (
-    <div className="flex flex-col md:flex-row gap-5 md:gap-24 mx-10 md:justify-center md:mt-20 lg:mt-24 xl:mt-28">
+    <div className="relative flex flex-col md:flex-row gap-5 md:gap-24 mx-10 md:justify-center md:mt-20 lg:mt-24 xl:mt-28">
       <div className="mx-auto rounded-full shadow-2xl max-w-[270px] max-h-[270px] md:max-w-xs md:max-h-[320px] md:mt-12 lg:mt-4 xl:mt-0">
         <img
           src="/photo.png"
@@ -21,7 +21,7 @@ export default function Hero() {
             />
           </span>
         </p>
-        <p className="text-justify text-slate-900 text-base sm:text-lg md:text-xl tracking-wide font-normal leading-normal">
+        <p className="text-justify text-slate-700 text-base sm:text-lg md:text-xl tracking-wide font-normal leading-normal">
           I am a fresh graduate in <b>Electrical Engineering</b> concentrating
           in <b>Computer Science</b>. I have knowledge of{" "}
           <b>JavaScript, Typescript, PHP</b> and <b>SQL </b>
@@ -40,7 +40,7 @@ export default function Hero() {
       <div className="absolute -z-10 top-1/3 left-3/4 md:left-[90%]">
         <div className="w-16 h-16 rounded-full bg-blue-300" />
       </div>
-      <div className="hidden md:block absolute -z-10 bottom-10 left-20">
+      <div className="hidden md:block absolute -z-10 -bottom-20 left-20">
         <div className="w-16 h-16 rounded-full bg-blue-300" />
       </div>
       <div className="hidden md:block absolute -z-10 top-3/4 left-3/4">
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { IoIosHome } from "react-icons/io";
+import { GiSkills } from "react-icons/gi";
 import { IoPerson } from "react-icons/io5";
 import { GrProjects } from "react-icons/gr";
 import { MdPermContactCalendar } from "react-icons/md";
@@ -22,11 +22,9 @@ export default function Navbar() {
 
   return (
     <div className="relative">
-      {!sideBar && (
-        <button className="m-4 p-2 md:hidden" onClick={() => setSideBar(true)}>
-          <GiHamburgerMenu size={30} />
-        </button>
-      )}
+      <button className="m-4 p-2 md:hidden" onClick={() => setSideBar(true)}>
+        <GiHamburgerMenu size={30} />
+      </button>
       <nav
         ref={navbarRef}
         className={`min-h-screen sm:w-64 w-48 flex flex-col absolute top-0 left-0 bg-slate-100 z-50 transition-transform ease-out transform  md:hidden ${
@@ -36,17 +34,7 @@ export default function Navbar() {
         <h3 className="p-4 font-bold text-xl">Navigation</h3>
         <a
           href="#"
-          className="mt-4 px-4 py-3 flex gap-2 items-center font-medium tracking-wider hover:bg-slate-200 sm:text-lg"
-          onClick={() => setSideBar(false)}
-        >
-          <span>
-            <IoIosHome size={25} />
-          </span>
-          Introduction
-        </a>
-        <a
-          href="#"
-          className="px-4 py-3 flex gap-2 items-center font-medium tracking-wider hover:bg-slate-200 sm:text-lg"
+          className="mt-4 px-4 py-4 flex gap-2 items-center font-medium tracking-wider hover:bg-slate-200 sm:text-lg"
           onClick={() => setSideBar(false)}
         >
           <span>
@@ -56,7 +44,7 @@ export default function Navbar() {
         </a>
         <a
           href="#"
-          className="px-4 py-3 flex gap-2 items-center font-medium tracking-wider hover:bg-slate-200 sm:text-lg"
+          className="px-4 py-4 flex gap-2 items-center font-medium tracking-wider hover:bg-slate-200 sm:text-lg"
           onClick={() => setSideBar(false)}
         >
           <span>
@@ -66,7 +54,17 @@ export default function Navbar() {
         </a>
         <a
           href="#"
-          className="px-4 py-3 flex gap-2 items-center font-medium tracking-wider hover:bg-slate-200 sm:text-lg"
+          className="px-4 py-4 flex gap-2 items-center font-medium tracking-wider hover:bg-slate-200 sm:text-lg"
+          onClick={() => setSideBar(false)}
+        >
+          <span>
+            <GiSkills size={25} />
+          </span>
+          Skills
+        </a>
+        <a
+          href="#"
+          className="px-4 py-4 flex gap-2 items-center font-medium tracking-wider hover:bg-slate-200 sm:text-lg"
           onClick={() => setSideBar(false)}
         >
           <span>
@@ -79,25 +77,25 @@ export default function Navbar() {
       <nav className="hidden md:flex justify-center items-center gap-16 lg:gap-24 xl:gap-32 w-full py-8">
         <a
           href="#"
-          className="flex gap-1 items-center font-bold tracking-wider text-lg lg:text-xl xl:text-2xl hover:underline"
-        >
-          Introduction
-        </a>
-        <a
-          href="#"
-          className="flex gap-1 items-center font-bold tracking-wider text-lg lg:text-xl xl:text-2xl hover:underline"
+          className="flex gap-1 items-center font-bold tracking-wider text-lg lg:text-xl xl:text-2xl rounded-full p-2 hover:bg-slate-300"
         >
           About me
         </a>
         <a
           href="#"
-          className="flex gap-1 items-center font-bold tracking-wider text-lg lg:text-xl xl:text-2xl hover:underline"
+          className="flex gap-1 items-center font-bold tracking-wider text-lg lg:text-xl xl:text-2xl rounded-full p-2 hover:bg-slate-300"
         >
           Projects
         </a>
         <a
           href="#"
-          className="flex gap-1 items-center font-bold tracking-wider text-lg lg:text-xl xl:text-2xl hover:underline"
+          className="flex gap-1 items-center font-bold tracking-wider text-lg lg:text-xl xl:text-2xl rounded-full p-2 hover:bg-slate-300"
+        >
+          Skills
+        </a>
+        <a
+          href="#"
+          className="flex gap-1 items-center font-bold tracking-wider text-lg lg:text-xl xl:text-2xl rounded-full p-2 hover:bg-slate-300"
         >
           Contact me
         </a>
