b8cecf2aee751dfdbc951be878effca12dbb6b57
Saputra <saputra.uta50@gmail.com>
2024-01-14 16:29:32 +0800

    fix navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -18,60 +18,90 @@ export default function Navbar() {
     return () => {
       document.removeEventListener("mousedown", navbarHandler);
     };
-  });
+  }, []);
 
   return (
     <div className="relative">
       {!sideBar && (
-        <button className="m-4 p-2" onClick={() => setSideBar(true)}>
+        <button className="m-4 p-2 md:hidden" onClick={() => setSideBar(true)}>
           <GiHamburgerMenu size={30} />
         </button>
       )}
-      {sideBar && (
-        <div ref={navbarRef} className="min-h-screen w-48 flex flex-col absolute top-0 left-0 bg-slate-100 z-50">
-          <h3 className="p-4 font-bold text-xl">Navigation</h3>
-          <a
-            href="#"
-            className="mt-4 px-4 py-3 flex gap-2 items-center font-medium tracking-wider hover:bg-slate-200"
-            onClick={() => setSideBar(false)}
-          >
-            <span>
-              <IoIosHome size={25} />
-            </span>
-            Introduction
-          </a>
-          <a
-            href="#"
-            className="px-4 py-3 flex gap-2 items-center font-medium tracking-wider hover:bg-slate-200"
-            onClick={() => setSideBar(false)}
-          >
-            <span>
-              <IoPerson size={25} />
-            </span>
-            About me
-          </a>
-          <a
-            href="#"
-            className="px-4 py-3 flex gap-2 items-center font-medium tracking-wider hover:bg-slate-200"
-            onClick={() => setSideBar(false)}
-          >
-            <span>
-              <GrProjects size={25} />
-            </span>
-            Projects
-          </a>
-          <a
-            href="#"
-            className="px-4 py-3 flex gap-2 items-center font-medium tracking-wider hover:bg-slate-200"
-            onClick={() => setSideBar(false)}
-          >
-            <span>
-              <MdPermContactCalendar size={25} />
-            </span>
-            Contact me
-          </a>
-        </div>
-      )}
+      <nav
+        ref={navbarRef}
+        className={`min-h-screen sm:w-64 w-48 flex flex-col absolute top-0 left-0 bg-slate-100 z-50 transition-transform ease-out transform  md:hidden ${
+          sideBar ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <h3 className="p-4 font-bold text-xl">Navigation</h3>
+        <a
+          href="#"
+          className="mt-4 px-4 py-3 flex gap-2 items-center font-medium tracking-wider hover:bg-slate-200 sm:text-lg"
+          onClick={() => setSideBar(false)}
+        >
+          <span>
+            <IoIosHome size={25} />
+          </span>
+          Introduction
+        </a>
+        <a
+          href="#"
+          className="px-4 py-3 flex gap-2 items-center font-medium tracking-wider hover:bg-slate-200 sm:text-lg"
+          onClick={() => setSideBar(false)}
+        >
+          <span>
+            <IoPerson size={25} />
+          </span>
+          About me
+        </a>
+        <a
+          href="#"
+          className="px-4 py-3 flex gap-2 items-center font-medium tracking-wider hover:bg-slate-200 sm:text-lg"
+          onClick={() => setSideBar(false)}
+        >
+          <span>
+            <GrProjects size={25} />
+          </span>
+          Projects
+        </a>
+        <a
+          href="#"
+          className="px-4 py-3 flex gap-2 items-center font-medium tracking-wider hover:bg-slate-200 sm:text-lg"
+          onClick={() => setSideBar(false)}
+        >
+          <span>
+            <MdPermContactCalendar size={25} />
+          </span>
+          Contact me
+        </a>
+      </nav>
+
+      <nav className="hidden md:flex justify-center items-center gap-16 lg:gap-24 xl:gap-32 w-full py-8">
+        <a
+          href="#"
+          className="flex gap-1 items-center font-bold tracking-wider text-lg lg:text-xl xl:text-2xl hover:underline"
+        >
+          Introduction
+        </a>
+        <a
+          href="#"
+          className="flex gap-1 items-center font-bold tracking-wider text-lg lg:text-xl xl:text-2xl hover:underline"
+        >
+          About me
+        </a>
+        <a
+          href="#"
+          className="flex gap-1 items-center font-bold tracking-wider text-lg lg:text-xl xl:text-2xl hover:underline"
+        >
+          Projects
+        </a>
+        <a
+          href="#"
+          className="flex gap-1 items-center font-bold tracking-wider text-lg lg:text-xl xl:text-2xl hover:underline"
+        >
+          Contact me
+        </a>
+      </nav>
     </div>
   );
 }
